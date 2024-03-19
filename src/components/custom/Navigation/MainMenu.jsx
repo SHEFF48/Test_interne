0534884cbb71ef9fb5c37fda78efe06f70d9240d
SHEFF48 <sun-service@ukr.net>
2024-03-19 12:43:43 +0200

    refactoring navbar

--- src/components/custom/Navigation/MainMenu.jsx
+++ src/components/custom/Navigation/MainMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import style from "./MainMenu.module.scss";
 
-const MainMenu = ({ menu }) => {
+const MainMenu = ({ menu, className }) => {
   return (
     <ul className={style.menuList}>
       {menu.map((menuItem) => (
